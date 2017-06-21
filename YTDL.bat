6d0e7a66dc32ed777ab3a72d34ca049b36072940
ATribeCalledQuest-Ce <slothman5339@gmail.com>
2017-06-21 04:23:06 -0700

    v1.1, added commented instructions for manual directory changers

--- YTDL.bat
+++ YTDL.bat
@@ -1,5 +1,8 @@
 @echo OFF
+REM Instructions for how to manually change the directory which the program will asume youtube-dl is in are provided both here in comments and in the README but it is recommended that you use batcreator.cs instead.
+REM change "c:" to whatever drive you want the folder to be in if you want it to be in your f drive change c: to f:
 c:
+REM change C:\ytdl to the directory in the drive stated above if you want to store youtube-dl in f:\ytdl instead of c:\ytdl, change it to f:\ytdl.
 cd C:\ytdl
 :loop
 set /p url=Paste the url of the video you want to download: 
