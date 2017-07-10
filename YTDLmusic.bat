8b4bc89de6b8165019eedfba7887d3b11dce79ee
ATribeCalledQuest-Ce <slothman5339@gmail.com>
2017-07-09 20:02:57 -0700

    updated to work with the new GUI

--- YTDLmusic.bat
+++ YTDLmusic.bat
@@ -4,8 +4,11 @@ REM change "c:" to whatever drive you want the folder to be in if you want it to
 REM c:
 REM change C:\ytdl to the directory in the drive stated above if you want to store youtube-dl in f:\ytdl instead of c:\ytdl, change it to f:\ytdl.
 cd media
+goto noargd
 :loop
 set /p url=Paste the url of the video you want to download: 
 REM remove "-f 140" if you want to download video not audio. ex. youtube-dl %url%
 youtube-dl -f 140 %url%
-goto loop
+:noargd
+IF "%~1"=="" GOTO loop
+youtube-dl -f 140 %*
